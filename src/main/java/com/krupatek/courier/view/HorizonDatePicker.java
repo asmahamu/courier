aca389a2258ff089d46e5442ab80e9098597d94c
Ashish Mahamuni <ashish531987@gmail.com>
2020-07-20 00:34:28 +0530

    Date changes in PODEntryForm

---
+++ src/main/java/com/krupatek/courier/view/HorizonDatePicker.java
@@ -0,0 +1,127 @@
+package com.krupatek.courier.view;
+
+import com.krupatek.courier.utils.DateUtils;
+import com.krupatek.courier.utils.NumberUtils;
+import com.vaadin.flow.component.Focusable;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
+
+import java.time.LocalDate;
+
+@Tag("object")
+public class HorizonDatePicker extends Div implements Focusable<TextField>{
+    private LocalDate currentDate = LocalDate.now();
+    private DateUtils dateUtils;
+    private TextField dayTF, monthTF, yearTF;
+    private DateChangeListener dateChangeListener;
+
+    public DateChangeListener getDateChangeListener() {
+        return dateChangeListener;
+    }
+
+    public void setDateChangeListener(DateChangeListener dateChangeListener) {
+        this.dateChangeListener = dateChangeListener;
+    }
+
+    public HorizonDatePicker(String label, LocalDate date, DateUtils dateUtils, NumberUtils numberUtils){
+        currentDate = date;
+        this.dateUtils = dateUtils;
+        setMaxWidth("175px");
+        VerticalLayout dateComponent = new VerticalLayout();
+        dateComponent.setPadding(false);
+        dateComponent.setMargin(false);
+        dateComponent.setSpacing(false);
+        dateComponent.setWidth("100%");
+
+        Label selectDateLbl = new Label(label);
+        selectDateLbl.setWidth("100%");
+
+        HorizontalLayout dateSubHL = new HorizontalLayout();
+        dateSubHL.setWidth("100%");
+        dayTF = new TextField("Day : ");
+        dayTF.setValueChangeMode(ValueChangeMode.LAZY);
+        dayTF.setWidth("33%");
+        dayTF.setAutoselect(true);
+        dayTF.setValue(String.valueOf(currentDate.getDayOfMonth()));
+        dayTF.addValueChangeListener(e -> {
+            boolean validDate = numberUtils.isDecimalNumber(e.getValue());
+            if(validDate) {
+                int value = Integer.parseInt(e.getValue()); // Check Range
+                validDate = 1 <= value && value <= currentDate.lengthOfMonth();
+                if(validDate) {
+                    currentDate = currentDate.withDayOfMonth(value);
+                    dateChangeListener.changedDate(currentDate);
+                    dayTF.setInvalid(false);
+                } else {
+                    dayTF.setInvalid(true);
+                    dayTF.setErrorMessage("Doesn't look like a day of a month");
+                }
+            }
+        });
+
+
+        monthTF = new TextField("Month : ");
+        monthTF.setWidth("33%");
+        monthTF.setAutoselect(true);
+        monthTF.setValue(String.valueOf(currentDate.getMonthValue()));
+        monthTF.setValueChangeMode(ValueChangeMode.LAZY);
+        monthTF.addValueChangeListener(e -> {
+            boolean validMonth = numberUtils.isDecimalNumber(e.getValue());
+            if(validMonth) {
+                int value = Integer.parseInt(e.getValue()); // Check Range
+                validMonth = 1 <= value && value <= 12;
+                if(validMonth) {
+                    currentDate = currentDate.withMonth(value);
+                    dateChangeListener.changedDate(currentDate);
+                    monthTF.setInvalid(false);
+                } else {
+                    monthTF.setInvalid(true);
+                    monthTF.setErrorMessage("Doesn't look like a month");
+                }
+            }
+        });
+
+        yearTF = new TextField("Year : ");
+        yearTF.setWidth("33%");
+        yearTF.setAutoselect(true);
+        yearTF.setValue(String.format("%02d", (currentDate.getYear() - 2000)));
+        yearTF.setValueChangeMode(ValueChangeMode.LAZY);
+        yearTF.addValueChangeListener(e -> {
+            boolean validYear = numberUtils.isDecimalNumber(e.getValue());
+            if(validYear) {
+                int value = Integer.parseInt(e.getValue()) + 2000; // Check Range
+                validYear = 2000 <= value && value <= 2099;
+                if(validYear) {
+                    currentDate = currentDate.withYear(value);
+                    dateChangeListener.changedDate(currentDate);
+                    yearTF.setInvalid(false);
+                } else {
+                    yearTF.setInvalid(true);
+                    yearTF.setErrorMessage("Doesn't look like a year");
+                }
+            }
+        });
+
+        dateSubHL.add(dayTF, monthTF, yearTF);
+
+        dateComponent.add(selectDateLbl, dateSubHL);
+
+        add(dateComponent);
+    }
+
+    @Override
+    public void focus() {
+        dayTF.focus();
+    }
+
+    public interface  DateChangeListener{
+        void changedDate(LocalDate date);
+    }
+// Add Builder
+
+}
